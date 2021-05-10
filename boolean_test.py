0446c2462e47c07fab61d2a7a0db1dd1d4157d90
ms-clancy <cs.mudaki@gmail.com>
2021-05-10 15:22:22 +0300

    added underscore in word application

--- boolean_test.py
+++ boolean_test.py
@@ -4,7 +4,7 @@ f = None
 # Random no added in my case I have used 8 instead of the initial 5
 
 for i in range(8):
-	with open ("application.log", "w") as f:
+	with open ("app_lication.log", "w") as f:
 		if i>2:
 			break
 print(f.closed)
