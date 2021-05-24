cefcf3cca23497c2ea536b1b6cba41a009fa428f
ms-clancy <cs.mudaki@gmail.com>
2021-05-24 13:04:01 +0300

    Changed Range to 14

--- boolean_test.py
+++ boolean_test.py
@@ -3,8 +3,8 @@ f = None
 
 # Random no added in my case I have used 8 instead of the initial 5
 
-for i in range(8):
+for i in range(14):
 	with open ("__application.log", "w") as f:
 		if i>2:
 			break
-print("Does file exist and has been closed succcesfully ? ", f.closed)
+print("Does file exist and has been closed succcesfully : ", f.closed)
